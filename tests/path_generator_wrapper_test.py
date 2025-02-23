cbd45850631db75730a6ca1bc716502b65a4edbb
Dean Benjamin <dben1182@gmail.com>
2025-02-23 13:16:55 -0700

    working

--- tests/path_generator_wrapper_test.py
+++ tests/path_generator_wrapper_test.py
@@ -10,56 +10,26 @@ tempPath = sys.path
 sys.path.insert(0,os.fspath(Path(__file__).parents[1]))
 tempPath = sys.path
 
-from source.wrappers.path_generator import Waypoint
-from source.wrappers.path_generator import WaypointData
-from source.wrappers.path_generator import PathGenerator
-from source.wrappers.path_generator import set_axes_equal
+from wrappers.path_generator_simplified import path_generator_simplified
 
-point_1 = np.array([[0],[0]])
-point_2 = np.array([[6],[0]])
+from wrappers.path_generator_simplified import Waypoint, WaypointData
 
-waypoint_1 = Waypoint(location=point_1)
-waypoint_2 = Waypoint(location=point_2)
-waypoint_1.velocity = np.array([[1],[0]])
-waypoint_2.velocity = np.array([[-1],[0]])
-waypoint_data = WaypointData(start_waypoint=waypoint_1,end_waypoint=waypoint_2)
+pathGen = path_generator_simplified(dimension=2)
 
-waypoints = waypoint_data.get_waypoint_locations()
+#creates the waypoints
+startWaypoint = Waypoint(location=np.array([[0.0],[0.0]]), velocity=np.array([[1.0],[0.0]]))
+endWaypoint = Waypoint(location=np.array([[10.0],[5.0]]), velocity=np.array([[0.0],[-2.0]]))
 
-max_curvature = 0.5
-max_incline = None
-order = 3
+waypoints = WaypointData(start_waypoint=startWaypoint,end_waypoint=endWaypoint)
 
-dimension = np.shape(point_1)[0]
+#creates the instance of the path generator
+pathGen = path_generator_simplified(dimension=2)
 
-path_gen = PathGenerator(dimension)
-start_time = time.time()
-control_points, status = path_gen.generate_path(waypoint_data=waypoint_data, max_curvature=max_curvature,
-    max_incline=max_incline, sfc_data=None, obstacles=None)
-end_time = time.time()
-# print("control_points: " , control_points)
-print("computation time:" , end_time - start_time)
-spline_start_time = 0
-scale_factor = 1
-bspline = BsplineEvaluation(control_points, order, spline_start_time, scale_factor, False)
-number_data_points = 10000
-spline_data, time_data = bspline.get_spline_data(number_data_points)
+#gets the data
+splineData, contrlPoints, timeData = pathGen.waypointPathGenerator(waypoints=waypoints,
+                                                                   max_curvature=100.0,
+                                                                   degree=3,
+                                                                   num_points_per_interval=100)
 
-spline_data, time_data = bspline.get_spline_data(1000)
-curvature_data, time_data = bspline.get_spline_curvature_data(1000)
-# acceleration_data, time_data = bspline.get_spline_derivative_data(1000,2)
-minvo_cps = bspline.get_minvo_control_points()
 
-# print("sfcs: " , sfcs)
-ax = plt.axes()
-ax.plot(spline_data[0,:], spline_data[1,:])
-ax.scatter(waypoints[0,:],waypoints[1,:])
-set_axes_equal(ax, dimension)
-plt.show()
 
-# plt.figure()
-# plt.plot(time_data, curvature_data)
-# plt.show()
-
-print("start curvature: " , curvature_data[0])
-print("end curvature: " , curvature_data[-1])
