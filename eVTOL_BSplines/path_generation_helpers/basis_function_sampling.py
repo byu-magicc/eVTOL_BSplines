edb8daf211b5009ca186c9545fefacd85ea810ab
Dean Benjamin <dben1182@gmail.com>
2025-04-21 16:08:32 -0600

    finished my dictionary

--- eVTOL_BSplines/path_generation_helpers/basis_function_sampling.py
+++ eVTOL_BSplines/path_generation_helpers/basis_function_sampling.py
@@ -2,16 +2,52 @@
 
 
 import numpy as np
-
-from matrix_generators_efficient import basisFunctionSampler
-
-
-
-
-
-sampler = basisFunctionSampler(degree=2,
-                               numSamplesPerSection=1000)
+from matrix_generators_efficient import basisFunctionSampler, readWriteBasisFunctions
+import matplotlib.pyplot as plt
+import os, sys
+from pathlib import Path
 
 
+writer = readWriteBasisFunctions(numSamplesPerSection=1000,
+                             highestDegree=5)
+
+
+
+temp1 = os.fspath(Path(__file__).parents[0])
+temp2 = os.path.abspath(os.path.join(temp1, 'lookUpTables/degree_5_basis.npz'))
+
+writer.writeToNpz(temp2)
+
+
+
+
+loadedList = writer.readFromNpz(filename=temp2)
+
+#creates the time array
+time = np.linspace(0.0,1.0,1000)
+
+
+#gets the figure and the axis
+figures, axes = plt.subplots(len(loadedList), 1, figsize=(6,8), sharex=True)
+
+#iterates over the list to plot the individual arrays
+for i in range(len(loadedList)):
+    #gets the current array
+    currentBasis = loadedList[i]
+    
+    #sets the number of lines in the basis function
+    numLines = i + 1
+
+    currentAxis = axes[i]
+    currentAxis.set_title("Basis function " + str(i))
+    for j in range(numLines):
+        
+        #gets the current array
+        currentArray = currentBasis[j,:]
+        #plots out from the axis
+        currentAxis.plot(time, currentArray, linewidth=2)
+    currentAxis.grid(True)
+
+plt.show()
 
 potato = 0
--- eVTOL_BSplines/path_generation_helpers/matrix_generators_efficient.py
+++ eVTOL_BSplines/path_generation_helpers/matrix_generators_efficient.py
@@ -37,9 +37,6 @@ class basisFunctionSampler:
     #creates the function to sample the basis function
     def sampleBasisFunction(self):
 
-
-
-
         #iterates over the number of subsections
         for i in range(self.numSections):
             
@@ -65,17 +62,80 @@ class basisFunctionSampler:
             tempList = np.array(tempList)
             #appends the temp list to the basis samples
             self.sampledList.append(tempList)
+        
+
+        #at the end, we create the 2d array of sampled points
+        self.sampledArray = np.ndarray((0,self.numSamplesPerSection))
+        for i in range(self.numSections):
+            
+            #gets the temp vector
+            tempVector = (self.sampledList)[i]
+
+            #reshapes the temp vector
+            reshapedVector = tempVector.reshape((1,self.numSamplesPerSection))
+
+            #appends to the sampled arrayu
+            self.sampledArray = np.concatenate((self.sampledArray, reshapedVector), axis=0)
 
 
 
-#creates a class to write to and read from the 
-class sample_and_store_basis_functions:
+#creates class to get list of all basis function storages 
 
-    #creates the initialization function
+class readWriteBasisFunctions:
+
+    #creates the initialization funciton
     def __init__(self,
-                 topDegree: int): #the highest degree of the basis functions from which to sample
-        self.topDegree = topDegree
+                 numSamplesPerSection: int, #the number of samples per section to be utilized tiwth this thing.
+                 highestDegree: int):#saves the highest degree of bspline from which to sample
 
+        self.highestDegree = highestDegree
+        #sets the number of basis types to get
+        self.numBasisTypes = self.highestDegree + 1
+        #saves the number of samples per section
+        self.numSamplesPerSection = numSamplesPerSection
+
+        self.sampledLists = []
+
+        potato = 0
+
+
+    #defines the function to write the basis functions samples out to an npy or npz file
+    def writeToNpz(self, fileName: str):
+        #iterates through and gets each of the samples
+        for i in range(self.numBasisTypes):
+            #gets the basis function sampler for this particular thing
+            currentSampler = basisFunctionSampler(degree=i,
+                                                  numSamplesPerSection=self.numSamplesPerSection)
+            
+            #gets the current sampler list
+            currentSamplerList = currentSampler.sampledArray
+
+            self.sampledLists.append(currentSamplerList)
+        np.savez(fileName, **{f"array_{i}": arr for i,arr in enumerate(self.sampledLists)})
 
     
+    #creates the function to read from Npz
+    def readFromNpz(self, filename: str):
+
+        loaded = np.load(filename)
+        #obtains the loaded list 
+        loaded_list = [loaded[f"array_{i}"] for i in range(self.numBasisTypes)]
+
+        #returns the loaded list
+        return loaded_list
+
+
+
+
+
+#creates the class to create the convolution
+class convolveBasisFunction:
+
+    def __init__(self,
+                 degree: int): #the degree of the basis polynomial we will be using
+        #saves the degree
+        self.degree = degree
+
+    #creates the 
+
 
