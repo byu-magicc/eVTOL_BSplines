64247d34b52521d101406f08cac4c868dd8f73a6
Dean Benjamin <dben1182@gmail.com>
2025-04-25 23:47:43 -0600

    added naieve S matrix

---
+++ eVTOL_BSplines/path_generation_helpers/S_integration_run.py
@@ -0,0 +1,13 @@
+
+
+import numpy as np
+from S_integration import S_integration
+
+
+
+
+integrator = S_integration(degree=5, M=40)
+
+
+
+potat = 0
--- eVTOL_BSplines/path_generation_helpers/intersections_integration.py
+++
@@ -1,2 +0,0 @@
-#this file implements the integration for the multiple sections and obtains
-#the S_M^d matrix
