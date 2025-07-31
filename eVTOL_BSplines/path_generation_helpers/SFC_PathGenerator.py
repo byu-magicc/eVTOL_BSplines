ebdd0993601bbeb57c826648d7556faa153e8ae4
Dean Benjamin <dben1182@gmail.com>
2025-07-31 17:02:50 -0600

    update

--- eVTOL_BSplines/path_generation_helpers/SFC_PathGenerator.py
+++ eVTOL_BSplines/path_generation_helpers/SFC_PathGenerator.py
@@ -4,3 +4,36 @@
 #it will be done by 
 
 import numpy as np
+
+#the importation of the Safe Flight Corridor, and paths of Safe Flight Corridors
+from submodules.path_generator.path_generation.safe_flight_corridor import SFC, SFC_Data
+#the importation of the waypoint, which refers to the position, acceleration, velocity, etc at a given time
+#and then WaypointData refers to a list of those, so like beginning and end positions with intermediate positions
+from submodules.path_generator.path_generation.waypoint_data import Waypoint, WaypointData
+#imports the function to get the matrix M which converts from standard control points to minvo control points
+from submodules.path_generator.path_generation.matrix_evaluation import get_M_matrix, evaluate_point_on_interval
+
+
+
+
+import time
+
+
+#creates the PathGenerator Class. This is really frustrating because I don't know exactly what's
+#supposed to be changed and what's the best way to make this work, and what can I modify?
+#and what should I not modify?
+class PathGenerator:
+
+
+    #creates the initialization function
+    def __init__(self,
+                 dimension: int,
+                 num_intervals_free_space: int = 5,
+                 degree: int = 3):
+        
+        #saves the dimension of this problem
+        self.dimension = dimension
+        #saves the degree of the polynomial B-Spline
+        self.degree = degree
+        #saves the M matrix, which is the Minvo matrix
+        self.M_d = get_M_matrix(order=degree)
---
+++ eVTOL_BSplines/path_generation_helpers/objective_functions.py
@@ -0,0 +1,8 @@
+#this file creates the objective function classs,
+#but of course we will be using our new Look up table based approach
+
+
+class ObjectiveFunction:
+
+
+    pass
